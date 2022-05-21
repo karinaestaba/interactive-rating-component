6e0d8e446fee64f12bc50e2b956760b35662bfb9
Karina Estaba <karina.estaba@gmail.com>
2022-05-21 03:06:45 -0400

    + Validación y simulación de envío de formulario.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,17 +1,23 @@
 window.addEventListener('load', (event) => {
-  let submitRatingBtn = document.getElementById('submitRating');
+  const submitRatingBtn = document.getElementById('submitRating');
+  const ratingForm = document.getElementById('fedbackRatingForm');
 
   if(submitRatingBtn != null) {
     submitRatingBtn.addEventListener('click', function(event) {
       event.preventDefault();
 
-      document.getElementById('fedbackRatingForm').classList.add('submited') 
-      document.getElementById('fedbackRatingFinish').classList.add('ready')
+      if(ratingForm.checkValidity()){
+        document.getElementById('fedbackRatingForm').classList.add('submited') 
+        document.getElementById('fedbackRatingFinish').classList.add('ready')
 
-      const optionSelected = document.querySelector('input[name="radioOption"]:checked');
+        const optionSelected = document.querySelector('input[name="radioOption"]:checked');
 
-      if (optionSelected != null) {
-        document.getElementById('rate-selected').innerText = optionSelected.value
+        if (optionSelected != null) {
+          document.getElementById('rate-selected').innerText = optionSelected.value
+        }
+      }else{
+        ratingForm.classList.add('invalid')
+        setTimeout(() => ratingForm.classList.remove('invalid'), 1000)
       }
     })
   }
